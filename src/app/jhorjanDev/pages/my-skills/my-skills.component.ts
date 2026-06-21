483bd5ba8b462045a5149d44e10de63545809557
jhorjanmartinez <jhorjanmartinez13@hotmail.com>
2026-06-21 18:07:29 -0500

    chore(ui): update home, about-me and skills sections with spanish copy

--- src/app/jhorjanDev/pages/my-skills/my-skills.component.ts
+++ src/app/jhorjanDev/pages/my-skills/my-skills.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 })
 export class MySkillsComponent {
   public skills: { name: string; level: number }[] = [
-    { name: 'Typescript', level: 3 },  // Avanzado
-    { name: 'Angular', level: 3 },     // Avanzado
-    { name: 'Nestjs', level: 2 },      // Medio
-    { name: 'PostgreSql', level: 2 },  // Medio
-    { name: 'TailwindCss', level: 3 }, // Avanzado
-    { name: 'Docker', level: 1 },      // Básico
+    { name: 'Typescript', level: 2 },
+    { name: 'Angular', level: 2 },
+    { name: 'Nestjs', level: 1 },
+    { name: 'PostgreSql', level: 1 },
+    { name: 'TailwindCss', level: 3 },
+    { name: 'Docker', level: 1 },
   ];
 }
